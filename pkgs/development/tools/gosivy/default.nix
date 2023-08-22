e3eebf2cd873aa442dfc33cb05c66203486d4bfa
aeaton <aeaton@batswireless.com>
2023-08-22 09:32:40 -0400

    added gosivy

--- pkgs/development/tools/gosivy/default.nix
+++ pkgs/development/tools/gosivy/default.nix
@@ -8,10 +8,10 @@ buildGoModule rec {
     rev = "v${version}";
     owner = "nakabonne";
     repo = "gosivy";
-    sha256 = "1jak1bg6k5iasscw68ra875k59k3iqhka2ykabsd427k1j3mypln";
+    sha256 = "676660f00993cf9adc25a572c89d25484bf5dc56";
   };
 
-  vendorSha256 = "1pwsc488ldw039by8nqpni801zry7dnf0rx4hhd73xpv2w7s8n2r";
+  vendorSha256 = "676660f00993cf9adc25a572c89d25484bf5dc56";
 
   doCheck = false;
 
