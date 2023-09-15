4b56c77be6b20be208bc727e29b770530373350e
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-09-15 14:06:42 +0200

    python3Packages.portend: 3.1.0 -> 3.2.0

--- pkgs/development/python-modules/portend/default.nix
+++ pkgs/development/python-modules/portend/default.nix
@@ -3,11 +3,11 @@
 
 buildPythonPackage rec {
   pname = "portend";
-  version = "3.1.0";
+  version = "3.2.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "239e3116045ea823f6df87d6168107ad75ccc0590e37242af0cc1e98c5d224e4";
+    sha256 = "sha256-UlCjUsGclZ12fKyHi4Kdk+XcdiWlFDOZoqANxmKP+3I=";
   };
 
   nativeBuildInputs = [ setuptools-scm ];
